3b58d23b002542add21de62aaf16898b6e00a855
czbalint <czanik.balint@gmail.com>
2021-04-15 23:24:22 +0200

    kesz v2.0 feltoltott verzio

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,5 +1,5 @@
 //=============================================================================================
-// Mintaprogram: Z�ld h�romsz�g. Ervenyes 2019. osztol.
+//Mintaprogram: Zold haromszog. Ervenyes 2019. osztol.
 //
 // A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
 // Tilos:
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    :
-// Neptun :
+// Nev    : Czanik Balint
+// Neptun : H7EEPG
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
@@ -31,8 +31,7 @@
 // Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
 // negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
 //=============================================================================================
-#include <fstream>
-#include <sstream>
+
 #include "framework.h"
 
 // vertex shader in GLSL
@@ -50,15 +49,21 @@ void main() {
     p = wLookAt + wRight * cCamWindowVertex.x + wUp * cCamWindowVertex.y;
 }
 )";
+
 // fragment shader in GLSL
 const char *fragmentSource = R"(
-#version 330
+    #version 330
 precision highp float;
-
+const int objFaces = 12;
+uniform vec3 wEye, v[20];
+uniform int planes[objFaces * 3];
+uniform vec3 kd, ks, F0;
+in vec3 p;
+out vec4 fragmentColor;
 const vec3 La = vec3(0.5f, 0.6f, 0.6f);
-const vec3 Le = vec3(0.8f, 0.8f, 0.8f);
-const vec3 lightPosition = vec3(0.0f, 0.0f, 0.0f);
-const vec3 ka = vec3(0.5f, 0.5f, 0.5f);
+const vec3 Le = vec3(1.2f, 0.5f, 1.8f);
+const vec3 lightPosition = vec3(0.2f, -0.2f, 0.25f);
+const vec3 ka = vec3(0.2f, 0.3f, 0.2f);
 const float shininess = 500.0f;
 const int maxdepth = 5;
 const float epsilon = 0.01f;
@@ -73,17 +78,18 @@ struct Ray {
     vec3 start, dir, weight;
 };
 
-const int objFaces = 12;
-uniform int top;
-uniform vec3 wEye, v[20];
-uniform int planes[objFaces * 3];
-uniform vec3 kd[2], ks[2], F0;
+struct Sphare{
+    vec3 center;
+    float radius;
+};
 
-void getObjPlane(int i, float scale, out vec3 p, out vec3 normal){
+
+
+void getObjPlane(int i, float scale, out vec3 pp, out vec3 normal){
     vec3 p1 = v[planes[3 * i] - 1], p2 = v[planes[3 * i + 1] - 1], p3 = v[planes[3 * i + 2] - 1];
     normal = cross(p2 - p1, p3 - p1);
     if (dot(p1, normal) < 0) normal = -normal;
-    p = p1 * scale + vec3(0, 0, 0.03f);
+    pp= p1 * scale + vec3(0, 0, 0.03f);
 }
 
 Hit intersectConvexPolyhedron(Ray ray, Hit hit, float scale, int mat){
@@ -104,98 +110,118 @@ Hit intersectConvexPolyhedron(Ray ray, Hit hit, float scale, int mat){
             }
         }
         if (!outside){
+            bool portal = true;
             for(int j = 0; j < objFaces; j++){
                 if (i == j) continue;
                 vec3 p11, n;
                 getObjPlane(j, scale, p11, n);
-
-                if (abs(dot(n, pintersect - p11)) < 0.1f) {
-                    hit.t = ti;
-                    hit.position = pintersect;
-                    hit.normal = normalize(normal);
-                    hit.mat = mat;
+                hit.t = ti;
+                hit.position = pintersect;
+                hit.normal = normalize(normal);
+                if ( abs(dot(n, pintersect - p11)) < 0.1f ) {
+                    hit.mat = 1;
+                    portal = false;
+                    break;
                 }
+            }
+            if (portal){
+                hit.mat = 3;
             }
         }
     }
     return hit;
 }
 
-Hit solveQuadratic(float a, float b, float c, Ray ray, Hit hit, float zmin, float zmax, float normz){
-    float discr = b * b - 4.0f * a * c;
-    if (discr >= 0){
-        float sqrt_discr = sqrt(discr);
-        float t1 = (-b + sqrt_discr) / 2.0f / a;
-        vec3 p = ray.start + ray.dir * t1;
-        if (p.z > zmax || p.z < zmin) t1 = -1;
-        float t2 = (-b - sqrt_discr) / 2.0f / a;
-        p = ray.start + ray.dir * t2;
-        if (t2 > 0 && (t2 < t1 || t1 < 0)) t1 = t2;
-        if (t1 > 0 && (t1 < hit.t || hit.t < 0)) {
-            hit.t = t1;
-            hit.position = ray.start + ray.dir * hit.t;
-            hit.normal = normalize(vec3(-hit.position.x, -hit.position.y, normz));
-            hit.mat = 2;
-        }
+Hit intersectImplicit(Ray ray, Hit besthit){
+    float a = 1.1f;
+    float b = 8.4f;
+    float c = 1.5f;
+    Hit hit;
+    hit.t = -1;
+    ray.dir = normalize(ray.dir);
+    float A, B, C;
+
+    A = a * ray.dir.x * ray.dir.x + b * ray.dir.y * ray.dir.y;
+    B = 2 * a * ray.start.x * ray.dir.x + 2 * b * ray.start.y * ray.dir.y - c * ray.dir.z;
+    C = a * ray.start.x * ray.start.x + b * ray.start.y * ray.start.y - c * ray.start.z;
+
+    float disc = (B*B) - (4 * A * C);
+    if (disc < 0) return besthit;
+    float t1 = (-B + sqrt(disc)) / (2 * A);
+    float t2 = (-B - sqrt(disc)) / (2 * A);
+
+    if (t1 <= 0) return besthit;
+    vec3 P1 = ray.start + ray.dir * t1;
+    vec3 P2 = ray.start + ray.dir * t2;
+    if (length(P1) > 0.3f && length(P2) < 0.3f) hit.t = t2;
+    if (length(P1) < 0.3f && length(P2) > 0.3f) hit.t = t1;
+    if (length(P1) < 0.3f && length(P2) < 0.3f){
+        hit.t = length(P1) < length(P2) ? t1 : t2;
     }
-    return hit;
-}
-
-Hit intersectMirascope(Ray ray, Hit hit) {
-    const float f = 0.25f;
-    const float H = 0.98f * f;
-
-    float a = dot(ray.dir.xy, ray.dir.xy);
-    float b = dot(ray.dir.xy, ray.start.xy) * 2 - 4 * f * ray.dir.z;
-    float c = dot(ray.start.xy, ray.start.xy) - 4 * f * ray.start.z;
-    hit = solveQuadratic(a,b,c,ray,hit,0,f/2,2*f);
-    if (top == 0) return hit;
-    b += 8 * f * ray.dir.z;
-    c += 8 * f * ray.start.z - 4 * f * f;
-    hit = solveQuadratic(a, b, c, ray, hit, f/2, H, -2 * f);
-    return hit;
+    hit.position = ray.start + ray.dir * hit.t;
+    vec3 F = vec3(1,0,(2 * a * hit.position.x) / c );
+    vec3 G = vec3(0,1,(2 * b * hit.position.y) / c );
+    hit.normal = -normalize(cross(F,G));
+    hit.mat = 2;
+    if (hit.t < besthit.t || besthit.t < 0){
+        return hit;
+    }
+    return besthit;
 }
 
 Hit firstIntersect(Ray ray) {
     Hit bestHit;
     bestHit.t = -1;
-    //bestHit = intersectMirascope(ray, bestHit);
-    //bestHit = intersectConvexPolyhedron(ray, bestHit, 0.02f, 0);
-    bestHit = intersectConvexPolyhedron(ray, bestHit, 1.73f, 1);
-    if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
+    bestHit = intersectImplicit(ray, bestHit);
+    bestHit = intersectConvexPolyhedron(ray, bestHit, 1.0f, 1);
+    if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal *= -1;
     return bestHit;
 }
 
 vec3 trace(Ray ray) {
     vec3 outRadiance = vec3(0, 0, 0);
-    for(int d = 0; d < maxdepth; d++) {
+    int  portalCount = 0;
+    int goldCont = 0;
+
+    while(portalCount < maxdepth && goldCont < maxdepth){
         Hit hit = firstIntersect(ray);
-        if (hit.t < 0) break;
         if (hit.mat < 2){
             vec3 lightdir = normalize(lightPosition - hit.position);
             float cosTheta = dot(hit.normal, lightdir);
             if (cosTheta > 0) {
                 vec3 LeIn = Le / dot(lightPosition - hit.position, lightPosition - hit.position);
-                outRadiance += ray.weight * LeIn * kd[hit.mat] * cosTheta;
+                outRadiance += ray.weight * LeIn * kd * cosTheta;
                 vec3 halfway = normalize(-ray.dir + lightdir);
                 float cosDelta = dot(hit.normal, halfway);
-                if (cosDelta > 0) outRadiance += ray.weight * LeIn * ks[hit.mat] * pow(cosDelta, shininess);
+                if (cosDelta > 0) outRadiance += ray.weight * LeIn * ks * pow(cosDelta, shininess);
             }
             ray.weight *= ka;
             outRadiance += ray.weight;
             return outRadiance;
         }
+        if (hit.mat == 2){
+            ray.weight *= F0 + (vec3(1, 1, 1) - F0) * pow(1 - dot(-ray.dir, hit.normal), 5);
+            ray.start = hit.position + hit.normal * epsilon;
+            ray.dir = reflect(ray.dir, hit.normal);
+            goldCont++;
+        }
 
-        ray.weight *= F0 + (vec3(1,1,1) - F0) * pow(dot(-ray.dir, hit.normal), 5);
-        ray.start = hit.position + hit.normal * epsilon;
-        ray.dir = reflect(ray.dir, hit.normal);
+        if (hit.mat == 3){
+            if (portalCount < maxdepth){
+                float alfa = 72.0f * 3.14 / 180;
+                ray.start = hit.position + hit.normal * epsilon;
+                ray.dir = reflect(ray.dir, hit.normal);
+                vec3 tmp = ray.start + ray.dir;
+                ray.start = ray.start * cos(alfa) + cross(hit.normal, ray.start) * sin(alfa) + hit.normal * dot(hit.normal, ray.start) * (1 - cos(alfa));
+                tmp = tmp * cos(alfa) + cross(hit.normal, tmp) * sin(alfa) + hit.normal * dot(hit.normal, tmp) * (1 - cos(alfa));
+                ray.dir = normalize(tmp - ray.start);
+                portalCount++;
+            }
+        }
     }
     outRadiance += ray.weight * La;
     return outRadiance;
 }
-
-in vec3 p;
-out vec4 fragmentColor;
 
 void main() {
     Ray ray;
@@ -206,11 +232,7 @@ void main() {
 }
 )";
 
-
-
-//---------------------------
 struct Camera {
-//---------------------------
 	vec3 eye, lookat, right, pvup, rvup;
 	float fov = 45 * (float)M_PI / 180;
 
@@ -226,17 +248,11 @@ struct Camera {
 	    eye = vec3(r * cos(t) + lookat.x, r * sin(t) + lookat.y, eye.z);
 	    set();
 	}
-	void Step(float step) {
-	    eye = normalize(eye + pvup * step) * length(eye);
-	    set();
-	}
+
 };
 
-
 Camera camera;
-bool animate = true;
-
-
+GPUProgram shader;
 
 float F(float n, float k) {
     return ((n-1)*(n-1) + k*k) / ((n+1)*(n+1)+k*k);
@@ -253,44 +269,8 @@ void onInitialization() {
 	 glEnableVertexAttribArray(0);
 	 glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,NULL);
 
-//	 shader.create(vertexSource,fragmentSource,"fragmentColor");
-//	 shader.setUniform(1, "top");
-//
-//	 const float g = 0.618f, G = 1.618f;
-//	 std::vector<vec3> v = {
-//	    vec3(0, g, G), vec3(0, -g, G), vec3(0, -g, -G), vec3(0, g, -G), vec3(G, 0, g), vec3(-G, 0, g), vec3(-G, 0, -g), vec3(G, 0, -g),
-//	    vec3(g,G,0), vec3(-g,G,0), vec3(-g,-G,0), vec3(g,-G,0), vec3(1,1,1), vec3(-1,1,1), vec3(-1,-1,1), vec3(1,-1,1),
-//	    vec3(1,-1,-1), vec3(1,1,-1), vec3(-1,1,-1), vec3(-1,-1,-1)
-//	 };
-//    for (int i = 0; i < v.size(); i++) shader.setUniform(v[i], "v[" + std::to_string(i) + "]");
-//
-//    std::vector<int> planes = {
-//        1,2,16, 1,13,9, 1,14,6, 2,15,11, 3,4,18, 3,17,12, 3,20,7, 19,10,9, 16,12,17, 5,8,18, 14,10,19, 6,7,20
-//    };
-//    for (int i = 0; i < planes.size(); i++) shader.setUniform(planes[i], "planes[" + std::to_string(i) + "]");
-//
-//    shader.setUniform(vec3(0.1f,0.2f,0.3f), "kd[0]");
-//    shader.setUniform(vec3(1.5f, 0.6f, 0.4f), "kd[1]");
-//    shader.setUniform(vec3(5,5,5), "ks[0]");
-//    shader.setUniform(vec3(1,1,1), "ks[1]");
-//    shader.setUniform(vec3(F(0.17,3.1), F(0.35,2.7), F(1.5,1.9)), "F0");
-
-}
-
-// Window has become invalid: Redraw
-void onDisplay() {
-    glViewport(0, 0, windowWidth, windowHeight);
-    std::ifstream vertexStream("vertex.glsl");
-    std::ifstream fragmentStream("fragman.glsl");
-    std::stringstream vertex;
-    std::stringstream fragment;
-    vertex << vertexStream.rdbuf();
-    fragment << fragmentStream.rdbuf();
-    GPUProgram shader(false);
-
-    shader.create(vertex.str().c_str(),fragment.str().c_str(),"fragmentColor");
-    //shader.setUniform(1, "top");
-     shader.Use();
+    shader.create(vertexSource,fragmentSource,"fragmentColor");
+    shader.Use();
     const float g = 0.618f, G = 1.618f;
     std::vector<vec3> v = {
             vec3(0, g, G), vec3(0, -g, G), vec3(0, -g, -G), vec3(0, g, -G), vec3(G, 0, g), vec3(-G, 0, g), vec3(-G, 0, -g), vec3(G, 0, -g),
@@ -304,29 +284,26 @@ void onDisplay() {
     };
     for (int i = 0; i < planes.size(); i++) shader.setUniform(planes[i], "planes[" + std::to_string(i) + "]");
 
-    shader.setUniform(vec3(0.1f,0.2f,0.3f), "kd[0]");
-    shader.setUniform(vec3(1.5f, 0.6f, 0.4f), "kd[1]");
-    shader.setUniform(vec3(5,5,5), "ks[0]");
-    shader.setUniform(vec3(1,1,1), "ks[1]");
+    shader.setUniform(vec3(1.3f, 0.2f, 0.2f), "kd");
+    shader.setUniform(vec3(1,1,1), "ks");
     shader.setUniform(vec3(F(0.17,3.1), F(0.35,2.7), F(1.5,1.9)), "F0");
+}
 
+// Window has become invalid: Redraw
+void onDisplay() {
     glClearColor(0,0,0,0);
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   shader.setUniform(camera.eye, "wEye");
-   shader.setUniform(camera.lookat, "wLookAt");
-   shader.setUniform(camera.right, "wRight");
-   shader.setUniform(camera.rvup, "wUp");
-   glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-   glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    shader.setUniform(camera.eye, "wEye");
+    shader.setUniform(camera.lookat, "wLookAt");
+    shader.setUniform(camera.right, "wRight");
+    shader.setUniform(camera.rvup, "wUp");
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glutSwapBuffers();
 }
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-    //if (key == 't') shader.setUniform(1,"top");
-    //if (key == 'T') shader.setUniform(0,"top");
-    if (key == 'f') camera.Step(0.1f);
-    if (key == 'F') camera.Step(-0.1f);
-    if (key == 'a') animate = !animate;
+
 }
 
 // Key of ASCII code released
@@ -340,10 +317,11 @@ void onMouse(int button, int state, int pX, int pY) {
 
 // Move mouse with key pressed
 void onMouseMotion(int pX, int pY) {
+
 }
 
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
-	if (animate) camera.Animate(glutGet(GLUT_ELAPSED_TIME) / 2000.0f);
+	camera.Animate(glutGet(GLUT_ELAPSED_TIME) / 2000.0f);
 	glutPostRedisplay();
 }
--- cmake-build-debug/fragman.glsl
+++ cmake-build-debug/fragman.glsl
@@ -1,11 +1,16 @@
 #version 330
 precision highp float;
-
+const int objFaces = 12;
+uniform vec3 wEye, v[20];
+uniform int planes[objFaces * 3];
+uniform vec3 kd, ks, F0;
+in vec3 p;
+out vec4 fragmentColor;
 const vec3 La = vec3(0.5f, 0.6f, 0.6f);
-const vec3 Le = vec3(1.5f, 1.5f, 0.8f);
-const vec3 lightPosition = vec3(0.2f, 0.1f, 0.2f);
-const vec3 ka = vec3(0.5f, 0.5f, 0.5f);
-const float shininess = 100.0f;
+const vec3 Le = vec3(1.2f, 0.5f, 1.8f);
+const vec3 lightPosition = vec3(0.2f, -0.2f, 0.25f);
+const vec3 ka = vec3(0.2f, 0.3f, 0.2f);
+const float shininess = 500.0f;
 const int maxdepth = 5;
 const float epsilon = 0.01f;
 
@@ -24,17 +29,13 @@ struct Sphare{
     float radius;
 };
 
-const int objFaces = 12;
-uniform int top;
-uniform vec3 wEye, v[20];
-uniform int planes[objFaces * 3];
-uniform vec3 kd[2], ks[2], F0;
 
-void getObjPlane(int i, float scale, out vec3 p, out vec3 normal){
+
+void getObjPlane(int i, float scale, out vec3 pp, out vec3 normal){
     vec3 p1 = v[planes[3 * i] - 1], p2 = v[planes[3 * i + 1] - 1], p3 = v[planes[3 * i + 2] - 1];
     normal = cross(p2 - p1, p3 - p1);
     if (dot(p1, normal) < 0) normal = -normal;
-    p = p1 * scale + vec3(0, 0, 0.03f);
+    pp= p1 * scale + vec3(0, 0, 0.03f);
 }
 
 Hit intersectConvexPolyhedron(Ray ray, Hit hit, float scale, int mat){
@@ -63,8 +64,8 @@ Hit intersectConvexPolyhedron(Ray ray, Hit hit, float scale, int mat){
                 hit.t = ti;
                 hit.position = pintersect;
                 hit.normal = normalize(normal);
-                if (abs(dot(n, pintersect - p11)) < 0.1f) {
-                    hit.mat = mat;
+                if ( abs(dot(n, pintersect - p11)) < 0.1f ) {
+                    hit.mat = 1;
                     portal = false;
                     break;
                 }
@@ -77,33 +78,6 @@ Hit intersectConvexPolyhedron(Ray ray, Hit hit, float scale, int mat){
     return hit;
 }
 
-Hit intersactSpahre(Ray ray, Hit besthit, vec3 cen){
-    Sphare object;
-    object.radius = 0.3f;
-    object.center = cen;
-    Hit hit;
-    hit.t = -1;
-    vec3 dist = ray.start - object.center;
-    float a = dot(ray.dir, ray.dir);
-    float b = dot(dist, ray.dir) * 2.0f;
-    float c = dot(dist, dist) - object.radius * object.radius;
-    float discr = b * b - 4.0f * a * c;
-    if (discr < 0) return besthit;
-    float sqrt_discr = sqrt(discr);
-    float t1 = (-b + sqrt_discr) / 2.0f / a;	// t1 >= t2 for sure
-    float t2 = (-b - sqrt_discr) / 2.0f / a;
-    if (t1 <= 0) return besthit;
-    hit.t = (t2 > 0) ? t2 : t1;
-    hit.position = ray.start + ray.dir * hit.t;
-    hit.normal = (hit.position - object.center) / object.radius;
-    hit.mat = 2;
-    if (hit.t < besthit.t || besthit.t < 0){
-        return hit;
-    }
-    return besthit;
-
-}
-
 Hit intersectImplicit(Ray ray, Hit besthit){
     float a = 1.1f;
     float b = 8.4f;
@@ -112,10 +86,6 @@ Hit intersectImplicit(Ray ray, Hit besthit){
     hit.t = -1;
     ray.dir = normalize(ray.dir);
     float A, B, C;
-
-//    A = (a * (ray.dir.x * ray.dir.x)) + (b * (ray.dir.y * ray.dir.y));
-//    B = 2 * a * ray.start.x * ray.dir.x + 2 * b * ray.start.y * ray.dir.y - c * ray.dir.z;
-//    C = a * (ray.start.x * ray.start.x) + b * (ray.start.y * ray.start.y) - c * ray.dir.z;
 
     A = a * ray.dir.x * ray.dir.x + b * ray.dir.y * ray.dir.y;
     B = 2 * a * ray.start.x * ray.dir.x + 2 * b * ray.start.y * ray.dir.y - c * ray.dir.z;
@@ -126,33 +96,18 @@ Hit intersectImplicit(Ray ray, Hit besthit){
     float t1 = (-B + sqrt(disc)) / (2 * A);
     float t2 = (-B - sqrt(disc)) / (2 * A);
 
-    vec3 pos1 = ray.start + ray.dir * t1;
-    if (t2 > 0) {
-        vec3 pos2 = ray.start + ray.dir * t2;
-        if (length(pos2) < 0.3f)
-        if (length(pos1) < 0.3f)
-        hit.t = (t2 < t1) ? t2 : t1;
-        else
-        hit.t = t2;
-        else if (length(pos1) < 0.3)
-        hit.t = t1;
-        else
-        return besthit;
-    } else if (length(pos1) < 0.3f)
-    hit.t = t1;
-    else
-    return besthit;
-
+    if (t1 <= 0) return besthit;
+    vec3 P1 = ray.start + ray.dir * t1;
+    vec3 P2 = ray.start + ray.dir * t2;
+    if (length(P1) > 0.3f && length(P2) < 0.3f) hit.t = t2;
+    if (length(P1) < 0.3f && length(P2) > 0.3f) hit.t = t1;
+    if (length(P1) < 0.3f && length(P2) < 0.3f){
+        hit.t = length(P1) < length(P2) ? t1 : t2;
+    }
     hit.position = ray.start + ray.dir * hit.t;
-
-//    if (t1 <= 0) return besthit;
-//    hit.t = (t2 > 0) ? t2 : t1;
-//    hit.position = dis + ray.dir * hit.t;
-    //if (distance(vec3(0,0,0), hit.position) > 0.3f) return besthit;
     vec3 F = vec3(1,0,(2 * a * hit.position.x) / c );
     vec3 G = vec3(0,1,(2 * b * hit.position.y) / c );
-    //hit.normal = -normalize(cross(F,G));
-    hit.normal = normalize(vec3(-2.0f * a * hit.position.x / c, -2.0f * b * hit.position.y / c, 1.0f));
+    hit.normal = -normalize(cross(F,G));
     hit.mat = 2;
     if (hit.t < besthit.t || besthit.t < 0){
         return hit;
@@ -160,72 +115,31 @@ Hit intersectImplicit(Ray ray, Hit besthit){
     return besthit;
 }
 
-//Hit solveQuadratic(float a, float b, float c, Ray ray, Hit hit, float zmin, float zmax, float normz){
-//    float discr = b * b - 4.0f * a * c;
-//    if (discr >= 0){
-//        float sqrt_discr = sqrt(discr);
-//        float t1 = (-b + sqrt_discr) / 2.0f / a;
-//        vec3 p = ray.start + ray.dir * t1;
-//        if (p.z > zmax || p.z < zmin) t1 = -1;
-//        float t2 = (-b - sqrt_discr) / 2.0f / a;
-//        p = ray.start + ray.dir * t2;
-//        if (t2 > 0 && (t2 < t1 || t1 < 0)) t1 = t2;
-//        if (t1 > 0 && (t1 < hit.t || hit.t < 0)) {
-//            hit.t = t1;
-//            hit.position = ray.start + ray.dir * hit.t;
-//            hit.normal = normalize(vec3(-hit.position.x, -hit.position.y, normz));
-//            hit.mat = 2;
-//        }
-//    }
-//    return hit;
-//}
-//
-//Hit intersectMirascope(Ray ray, Hit hit) {
-//    const float f = 0.25f;
-//    const float H = 0.98f * f;
-//
-//    float a = dot(ray.dir.xy, ray.dir.xy);
-//    float b = dot(ray.dir.xy, ray.start.xy) * 2 - 4 * f * ray.dir.z;
-//    float c = dot(ray.start.xy, ray.start.xy) - 4 * f * ray.start.z;
-//    hit = solveQuadratic(a,b,c,ray,hit,0,f/2,2*f);
-//    if (top == 0) return hit;
-//    b += 8 * f * ray.dir.z;
-//    c += 8 * f * ray.start.z - 4 * f * f;
-//    hit = solveQuadratic(a, b, c, ray, hit, f/2, H, -2 * f);
-//    return hit;
-//}
-
 Hit firstIntersect(Ray ray) {
     Hit bestHit;
     bestHit.t = -1;
-    //bestHit = intersectMirascope(ray, bestHit);
-    //bestHit = intersectConvexPolyhedron(ray, bestHit, 0.02f, 0);
     bestHit = intersectImplicit(ray, bestHit);
-    //bestHit = intersactSpahre(ray, bestHit, vec3(0,0,0));
-    //bestHit = intersactSpahre(ray, bestHit, vec3(0.7,0.7,0.7));
-    //abestHit = intersactSpahre(ray, bestHit, vec3(-0.7,-0.7,-0.7));
     bestHit = intersectConvexPolyhedron(ray, bestHit, 1f, 1);
-
-    if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
+    if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal *= -1;
     return bestHit;
 }
 
 vec3 trace(Ray ray) {
     vec3 outRadiance = vec3(0, 0, 0);
     int  portalCount = 0;
+    int goldCont = 0;
 
-    for(int d = 0; d < maxdepth; d++) {
+    while(portalCount < maxdepth && goldCont < maxdepth){
         Hit hit = firstIntersect(ray);
-        //if (hit.t < 0) break;
         if (hit.mat < 2){
             vec3 lightdir = normalize(lightPosition - hit.position);
             float cosTheta = dot(hit.normal, lightdir);
             if (cosTheta > 0) {
                 vec3 LeIn = Le / dot(lightPosition - hit.position, lightPosition - hit.position);
-                outRadiance += ray.weight * LeIn * kd[hit.mat] * cosTheta;
+                outRadiance += ray.weight * LeIn * kd * cosTheta;
                 vec3 halfway = normalize(-ray.dir + lightdir);
                 float cosDelta = dot(hit.normal, halfway);
-                if (cosDelta > 0) outRadiance += ray.weight * LeIn * ks[hit.mat] * pow(cosDelta, shininess);
+                if (cosDelta > 0) outRadiance += ray.weight * LeIn * ks * pow(cosDelta, shininess);
             }
             ray.weight *= ka;
             outRadiance += ray.weight;
@@ -235,7 +149,7 @@ vec3 trace(Ray ray) {
             ray.weight *= F0 + (vec3(1, 1, 1) - F0) * pow(1 - dot(-ray.dir, hit.normal), 5);
             ray.start = hit.position + hit.normal * epsilon;
             ray.dir = reflect(ray.dir, hit.normal);
-
+            goldCont++;
         }
 
         if (hit.mat == 3){
@@ -248,19 +162,14 @@ vec3 trace(Ray ray) {
                 tmp = tmp * cos(alfa) + cross(hit.normal, tmp) * sin(alfa) + hit.normal * dot(hit.normal, tmp) * (1 - cos(alfa));
                 ray.dir = normalize(tmp - ray.start);
                 portalCount++;
-            } else {
-                break;
             }
-            d--;
-            continue;
         }
     }
     outRadiance += ray.weight * La;
     return outRadiance;
 }
 
-in vec3 p;
-out vec4 fragmentColor;
+
 
 void main() {
     Ray ray;
